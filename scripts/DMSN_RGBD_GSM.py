593a2551c5733ada1f001184243f6fea886915a0
ihssane <ihssanehouhou@gmail.com>
2023-01-20 18:31:38 +0100

    Add files via upload

---
+++ scripts/DMSN_RGBD_GSM.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 2018
+
+@author: longang
+"""
+
+# get_ipython().magic(u'load_ext autoreload')
+# get_ipython().magic(u'autoreload 2')
+
+import numpy as np
+import tensorflow as tf
+import random as rn
+import os, sys
+
+
+# set current working directory
+cur_dir = os.getcwd()
+os.chdir(cur_dir)
+sys.path.append(cur_dir)
+
+# =============================================================================
+#  For reprodocable results
+# =============================================================================
+os.environ['PYTHONHASHSEED'] = '0'
+np.random.seed(42)
+rn.seed(12345)
+session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+from tensorflow.keras.models import load_model, Model
+from tensorflow.python.keras import backend as K
+tf.random.set_seed(1234)
+sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+import keras, glob
+from keras.preprocessing import image as kImage
+from sklearn.utils import compute_class_weight
+from DMSN_module_RGBD_Depth_WIL import DMSN_module
+from keras.utils.data_utils import get_file
+
+# alert the user
+if keras.__version__!= '2.0.6' or tf.__version__!='1.1.0' or sys.version_info[0]<3:
+    print('We implemented using [keras v2.0.6, tensorflow-gpu v1.1.0, python v3.6.3], other versions than these may cause errors somehow!\n')
+
+# =============================================================================
+# Few frames, load into memory directly
+# =============================================================================
+def getData(train_dir, dataset_dir, scene, dataset_dir_depth):
+    
+    void_label = -1.
+    
+    Y_list = glob.glob(os.path.join(train_dir,'*.png'))
+    
+    if scene in ['CAVIAR2', 'Foliage']:
+        ex = '*.jpg'
+    else:
+        ex = '*.png'
+        
+    # print(dataset_dir)
+    X_list = glob.glob(os.path.join(dataset_dir, ex))
+    X_list_depth = glob.glob(os.path.join(dataset_dir_depth, ex))
+    if len(Y_list)<=0 or len(X_list)<=0:
+        raise ValueError('System cannot find the dataset path or ground-truth path. Please give the correct path.')
+        
+    
+    if len(X_list)!=len(Y_list):
+        raise ValueError('The number of X_list and Y_list must be equal.')
+        
+    # X must be corresponded to Y
+    X_list = sorted(X_list)
+    X_list_depth = sorted(X_list_depth)
+    Y_list = sorted(Y_list)
+    
+    # process training images
+    X = []
+    Depth = []
+    Y = []
+    for i in range(0, len(X_list)):
+        x = kImage.load_img(X_list[i])
+        x = kImage.img_to_array(x)
+        X.append(x)
+
+        x = kImage.load_img(X_list_depth[i], grayscale = True)
+        x = kImage.img_to_array(x)
+        Depth.append(x)
+        
+        x = kImage.load_img(Y_list[i], grayscale = True)
+        x = kImage.img_to_array(x)
+        x[x==1.] = 255. # some ground-truths in this dataset contain values of [0,1]
+        shape = x.shape
+        x /= 255.0
+        x = x.reshape(-1)
+        idx = np.where(np.logical_and(x>0.25, x<0.8))[0] # find non-ROI
+        if (len(idx)>0):
+            x[idx] = void_label
+        x = x.reshape(shape)
+        x = np.floor(x)
+        Y.append(x)
+        
+    X = np.asarray(X)
+    Depth = np.asarray(Depth)
+    Y = np.asarray(Y)
+
+    # Shuffle the training data
+    idx = list(range(X.shape[0]))
+    np.random.shuffle(idx)
+    np.random.shuffle(idx)
+    X = X[idx]
+    Depth = Depth[idx]
+    Y = Y[idx]
+    
+    # compute class weights
+    cls_weight_list = []
+    for i in range(Y.shape[0]):
+        y = Y[i].reshape(-1)
+        idx = np.where(y!=void_label)[0]
+        if(len(idx)>0):
+            y = y[idx]
+        lb = np.unique(y) #  0., 1
+        cls_weight = compute_class_weight('balanced', lb , y)
+        class_0 = cls_weight[0]
+        class_1 = cls_weight[1] if len(lb)>1 else 1.0
+        
+        cls_weight_dict = {0:class_0, 1: class_1}
+        cls_weight_list.append(cls_weight_dict)
+        
+    cls_weight_list = np.asarray(cls_weight_list)
+
+    return[X, Y, cls_weight_list, Depth]
+    
+    
+def train(data, scene, mdl_path, vgg_weights_path):
+    
+    ### hyper-params
+    lr = 1e-4
+    val_split = 0.2
+    max_epoch = 100
+    batch_size = 1
+    ###
+    
+    img_shape = data[0][0].shape #(height, width, channel)
+    model = MFCN_module(lr, img_shape, scene, vgg_weights_path)
+    model = model.initModel('SBM')
+
+    # make sure that training input shape equals to model output
+    input_shape = (img_shape[0], img_shape[1])
+    output_shape = (model.output._keras_shape[1], model.output._keras_shape[2])
+    assert input_shape==output_shape, 'Given input shape:' + str(input_shape) + ', but your model outputs shape:' + str(output_shape)
+    
+    early = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=30, verbose=0, mode='auto')
+    redu = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=0, mode='auto')
+    model.fit([data[0], data[3]], data[1], 
+              validation_split=val_split,
+              epochs=max_epoch, batch_size=batch_size, 
+              callbacks=[redu, early], verbose=1, class_weight=data[2], shuffle = True)
+    
+    model.save(mdl_path)
+    del model, data, early, redu
+
+
+
+# =============================================================================
+# Main func
+# =============================================================================
+dataset = {
+            'S_1',
+            'S_2',
+            'S_3',
+            'S_4',
+            'S_5',
+            'S_6',
+            'S_7'
+            }
+
+
+main_dir = os.path.join('..', 'DMSN_Depth_WIL')
+vgg_weights_path = 'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+if not os.path.exists(vgg_weights_path):
+    WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    vgg_weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                WEIGHTS_PATH_NO_TOP, cache_subdir='models',
+                                file_hash='6d6bbae143d832006294945121d1f1fc')
+
+main_mdl_dir = os.path.join(main_dir, 'GSM', 'models')
+if not os.path.exists(main_mdl_dir):
+    os.makedirs(main_mdl_dir)
+   
+for scene in dataset:
+    print ('Training ->>> ' + scene)
+    
+    train_dir = os.path.join('..', 'datasets','GSM_dataset', scene, 'train', 'GT')
+    dataset_dir = os.path.join('..', 'datasets','GSM_dataset', scene, 'train', 'RGB')
+    dataset_dir_depth = os.path.join('..', 'datasets','GSM_dataset', scene, 'train', 'DEPTH')
+
+    mdl_path = os.path.join(main_mdl_dir, 'mdl_GSM_RGBD_Depth_WIL_'+ str(scene) + '.h5')
+
+    results = getData(train_dir, dataset_dir, scene, dataset_dir_depth)
+    train(results, scene, mdl_path, vgg_weights_path)
+    del results
---
+++ scripts/DMSN_module_RGBD_One_IL.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 2018
+
+@author: longang
+"""
+
+import keras
+from keras.models import Model
+from keras.layers import Input, Dropout, Activation, SpatialDropout2D
+from keras.layers.convolutional import Conv2D, Cropping2D, UpSampling2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers import concatenate, add, multiply, Subtract
+from my_upsampling_2d import MyUpSampling2D
+from instance_normalization import InstanceNormalization
+import keras.backend as K
+import tensorflow as tf
+
+def loss(y_true, y_pred):
+    void_label = -1.
+    y_pred = K.reshape(y_pred, [-1])
+    y_true = K.reshape(y_true, [-1])
+    idx = tf.where(tf.not_equal(y_true, tf.constant(void_label, dtype=tf.float32)))
+    y_pred = tf.gather_nd(y_pred, idx) 
+    y_true = tf.gather_nd(y_true, idx)
+    return K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+
+def acc(y_true, y_pred):
+    void_label = -1.
+    y_pred = tf.reshape(y_pred, [-1])
+    y_true = tf.reshape(y_true, [-1])
+    idx = tf.where(tf.not_equal(y_true, tf.constant(void_label, dtype=tf.float32)))
+    y_pred = tf.gather_nd(y_pred, idx) 
+    y_true = tf.gather_nd(y_true, idx)
+    return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
+
+def loss2(y_true, y_pred):
+    return K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+
+def acc2(y_true, y_pred):
+    return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
+
+class DMSN_module(object):
+    
+    def __init__(self, lr, img_shape, scene, vgg_weights_path):
+        self.lr = lr
+        self.img_shape = img_shape
+        self.scene = scene
+        self.vgg_weights_path = vgg_weights_path
+        self.method_name = 'DMSN'
+        
+    def VGG16(self, x): 
+        
+        # Block 1
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', data_format='channels_last')(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+        
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+        F1 = x
+    
+        # Block 2
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+        
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+        F2 = x
+    
+        # Block 3
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+        F3 = x
+    
+        # Block 4
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+        F4 = x
+
+        # Block 5
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+        F5 = x
+        
+        return F1, F2, F3, F4, F5
+
+    def VGG16_Depth(self, x): 
+        
+        # Block 1
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='d_block1_conv1', data_format='channels_last')(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='d_block1_conv2')(x)
+        
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='d_block1_pool')(x)
+        d_F1 = x
+    
+        # Block 2
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_block2_conv1')(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_block2_conv2')(x)
+        
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='d_block2_pool')(x)
+        d_F2 = x
+    
+        # Block 3
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='d_block3_conv1')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='d_block3_conv2')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='d_block3_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='d_block3_pool')(x)
+        d_F3 = x
+    
+        # Block 4
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block4_conv1')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block4_conv2')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block4_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='d_block4_pool')(x)
+        d_F4 = x
+
+        # Block 5
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block5_conv1')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block5_conv2')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='d_block5_conv3')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='d_block5_pool')(x)
+        d_F5 = x
+        
+        return d_F1, d_F2, d_F3, d_F4, d_F5
+    
+    def decoder(self, X1, X2, X3, X4, X5, C1, C2, C3, C4, C5):
+        DC5 = concatenate([X5, C5], name='DC_X5_C5')
+        D5 = Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='relu', padding='same', name='D5')(DC5)
+
+        DC4 = concatenate([X4, C4, D5], name='DC_X4_C4')
+        D4 = Conv2DTranspose(256, (3, 3), strides=(2, 2), activation='relu', padding='same', name='D4')(DC4)
+
+        DC3 = concatenate([X3, C3, D4], name='DC_X3_C3')
+        D3 = Conv2DTranspose(384, (3, 3), strides=(2, 2), activation='relu', padding='same', name='D3')(DC3)
+
+        DC2 = concatenate([X2, C2, D3], name='DC_X2_C2')
+        D2 = Conv2DTranspose(512, (3, 3), strides=(2, 2), activation='relu', padding='same', name='D2')(DC2)
+
+        DC1 = concatenate([X1, C1, D2], name='DC_X1_C1')
+        D1 = Conv2DTranspose(640, (3, 3), strides=(2, 2), activation='relu', padding='same', name='D1')(DC1)
+
+        x = Conv2D(1, 1, strides=(1, 1), activation='sigmoid')(D1)
+        return x
+    
+    #InterMediat Skeep Connection
+    # def IMSC_D(self, d_F1, d_F2, d_F3, d_F4, d_F5):
+
+    #     d_X1 = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_X1_conv')(d_F1)
+    #     d_AvX1 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='d_APX1')(d_X1)
+    #     d_C1 = Subtract()([d_X1, d_AvX1])
+    #     d_X2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_X2_conv')(d_F2)
+    #     d_AvX2 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='d_APX2')(d_X2)
+    #     d_C2 = Subtract()([d_X2, d_AvX2])
+    #     d_X3 = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_X3_conv')(d_F3)
+    #     d_AvX3 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='d_APX3')(d_X3)
+    #     d_C3 = Subtract()([d_X3, d_AvX3])
+    #     d_X4 = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_X4_conv')(d_F4)
+    #     d_AvX4 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='d_APX4')(d_X4)
+    #     d_C4 = Subtract()([d_X4, d_AvX4])
+    #     d_X5 = Conv2D(128, (3, 3), activation='relu', padding='same', name='d_X5_conv')(d_F5)
+    #     d_AvX5 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='d_APX5')(d_X5)
+    #     d_C5 = Subtract()([d_X5, d_AvX5])
+        
+        
+    #     return d_X1, d_X2, d_X3, d_X4, d_X5, d_C1, d_C2, d_C3, d_C4, d_C5
+
+    #InterMediat Skeep Connection
+    def IMSC(self, F1, F2, F3, F4, F5, d_F1, d_F2, d_F3, d_F4, d_F5):
+
+        CF1 = concatenate([F1, d_F1], name='DC_F1_d_F1')
+
+        X1 = Conv2D(128, (3, 3), activation='relu', padding='same', name='X1_conv')(CF1)
+        AvX1 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='APX1')(X1)
+        C1 = Subtract()([X1, AvX1])
+
+        CF2 = concatenate([F2, d_F2], name='DC_F2_d_F2')
+        X2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='X2_conv')(CF2)
+        AvX2 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='APX2')(X2)
+        C2 = Subtract()([X2, AvX2])
+
+        CF3 = concatenate([F3, d_F3], name='DC_F3_d_F3')
+        X3 = Conv2D(128, (3, 3), activation='relu', padding='same', name='X3_conv')(CF3)
+        AvX3 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='APX3')(X3)
+        C3 = Subtract()([X3, AvX3])
+
+        CF4 = concatenate([F4, d_F4], name='DC_F4_d_F4')
+        X4 = Conv2D(128, (3, 3), activation='relu', padding='same', name='X4_conv')(CF4)
+        AvX4 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='APX4')(X4)
+        C4 = Subtract()([X4, AvX4])
+
+        CF5 = concatenate([F5, d_F5], name='DC_F5_d_F5')
+        X5 = Conv2D(128, (3, 3), activation='relu', padding='same', name='X5_conv')(CF5)
+        AvX5 = AveragePooling2D((3, 3), strides=(1, 1), padding='same', name='APX5')(X5)
+        C5 = Subtract()([X5, AvX5])
+        
+        
+        return X1, X2, X3, X4, X5, C1, C2, C3, C4, C5    
+    
+    def initModel(self, dataset_name):
+        assert dataset_name in ['CDnet', 'SBI', 'UCSD', 'SBM'], 'dataset_name must be either one in ["CDnet", "SBI", "UCSD", "SBM"]]'
+        assert len(self.img_shape)==3
+        h, w, d = self.img_shape
+        
+        net_input = Input(shape=(h, w, d), name='net_input')
+        vgg_output = self.VGG16(net_input)
+        model = Model(inputs=net_input, outputs=vgg_output, name='model')
+        model.load_weights(self.vgg_weights_path, by_name=True)
+        
+        unfreeze_layers = ['block5_conv1','block5_conv2', 'block5_conv3']
+        for layer in model.layers:
+            if(layer.name not in unfreeze_layers):
+                layer.trainable = False
+                
+        F1, F2, F3, F4, F5 = model.output
+
+        net_input_depth = Input(shape=(h, w, 1), name='net_input_depth')
+        vgg_output_depth = self.VGG16_Depth(net_input_depth)
+        model_depth = Model(inputs=net_input_depth, outputs=vgg_output_depth, name='model_depth')
+        model_depth.load_weights(self.vgg_weights_path, by_name=True)
+        
+        unfreeze_layers = ['d_block5_conv1','d_block5_conv2', 'd_block5_conv3']
+        for layer in model_depth.layers:
+            if(layer.name not in unfreeze_layers):
+                layer.trainable = False
+                
+        d_F1, d_F2, d_F3, d_F4, d_F5 = model_depth.output
+        
+        # pad in case of CDnet2014
+        if dataset_name=='CDnet':
+            x1_ups = {'streetCornerAtNight':(0,1), 'tramStation':(1,0), 'turbulence2':(1,0)}
+            for key, val in x1_ups.items():
+                if self.scene==key:
+                    # upscale by adding number of pixels to each dim.
+                    x = MyUpSampling2D(size=(1,1), num_pixels=val, method_name = self.method_name)(x)
+                    break
+                
+        X1, X2, X3, X4, X5, C1, C2, C3, C4, C5 = self.IMSC(F1, F2, F3, F4, F5, d_F1, d_F2, d_F3, d_F4, d_F5)
+        x = self.decoder(X1, X2, X3, X4, X5, C1, C2, C3, C4, C5)
+        
+        # pad in case of CDnet2014
+        if dataset_name=='CDnet':
+            if(self.scene=='tramCrossroad_1fps'):
+                x = MyUpSampling2D(size=(1,1), num_pixels=(2,0), method_name=self.method_name)(x)
+            elif(self.scene=='bridgeEntry'):
+                x = MyUpSampling2D(size=(1,1), num_pixels=(2,2), method_name=self.method_name)(x)
+            elif(self.scene=='fluidHighway'):
+                x = MyUpSampling2D(size=(1,1), num_pixels=(2,0), method_name=self.method_name)(x)
+            elif(self.scene=='streetCornerAtNight'): 
+                x = MyUpSampling2D(size=(1,1), num_pixels=(1,0), method_name=self.method_name)(x)
+                x = Cropping2D(cropping=((0, 0),(0, 1)))(x)
+            elif(self.scene=='tramStation'):  
+                x = Cropping2D(cropping=((1, 0),(0, 0)))(x)
+            elif(self.scene=='twoPositionPTZCam'):
+                x = MyUpSampling2D(size=(1,1), num_pixels=(0,2), method_name=self.method_name)(x)
+            elif(self.scene=='turbulence2'):
+                x = Cropping2D(cropping=((1, 0),(0, 0)))(x)
+                x = MyUpSampling2D(size=(1,1), num_pixels=(0,1), method_name=self.method_name)(x)
+            elif(self.scene=='turbulence3'):
+                x = MyUpSampling2D(size=(1,1), num_pixels=(2,0), method_name=self.method_name)(x)
+
+        vision_model = Model(inputs=[net_input, net_input_depth], outputs=x, name='vision_model')
+        opt = keras.optimizers.RMSprop(lr = self.lr, rho=0.9, epsilon=1e-08, decay=0.)
+        
+        # Since UCSD has no void label, we do not need to filter out
+        if dataset_name == 'UCSD':
+            c_loss = loss2
+            c_acc = acc2
+        else:
+            c_loss = loss
+            c_acc = acc
+        
+        vision_model.compile(loss=c_loss, optimizer=opt, metrics=[c_acc])
+        return vision_model
